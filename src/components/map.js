9eaf85b711f47c8f1051e342098a1be0fc637555
Ghassen-Da <ghassendaoud99@gmail.com>
2023-11-27 16:20:01 +0100

    review changes

--- src/components/map.js
+++ src/components/map.js
@@ -66,7 +66,7 @@ export default function Map(props) {
                 onClick={()=>handleDialogOpen(station)}
               />      
             )
-            return null
+          return null
           }
           )
         }
